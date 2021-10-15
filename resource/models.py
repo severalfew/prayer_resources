0bf0ba4055f39e0799b687c6f67e4887c7a20338
Stuart Nygard <stuart@severalfew.com>
2021-10-14 20:28:13 -0600

    exploratory model changes

---
+++ resource/models.py
@@ -0,0 +1,35 @@
+from django.db import models
+
+from wagtail.admin.edit_handlers import FieldPanel
+from wagtail.core.fields import RichTextField
+from wagtail.core.models import Page
+
+class CuratorIndexPage(Page):
+    intro = RichTextField(blank=True)
+
+    name = models.CharField(max_length=100)
+    link = models.UrlField()
+    image = modles.ImageField()
+
+class ResourceIndexPage(Page):
+    intro = RichTextField(blank=True)
+    
+    content_panels = Page.content_panels + [
+        FieldPanel('intro', classname="full")
+    ]
+
+class ResourcePage(Page):
+    date = models.DateField("Post Date")
+    intro = models.CharField(max_length=250)
+    description = RichTextField(blank=True)
+
+    search_fields = Page.search_fields + [
+        index.SearchField('intro'),
+        index.SearchField('body'),
+    ]
+
+    content_panels = Page.content_panels + [
+        FieldPanel('date'),
+        FieldPanel('intro'),
+        FieldPanel('body', classname="full"),
+    ]
