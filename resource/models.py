3de55654a13dd9a7201fcd739e18b252112c0638
Stuart Nygard <stuart@severalfew.com>
2021-10-15 07:28:38 -0600

    remove idea from git; add CuratorIndexPage

--- resource/models.py
+++ resource/models.py
@@ -4,19 +4,30 @@ from wagtail.admin.edit_handlers import FieldPanel
 from wagtail.core.fields import RichTextField
 from wagtail.core.models import Page
 
+
 class CuratorIndexPage(Page):
+    intro = RichTextField(blank=True)
+
+    content_panels = Page.content_panels + [
+        FieldPanel('intro', classname="full")
+    ]
+
+
+class CuratorPage(Page):
     intro = RichTextField(blank=True)
 
     name = models.CharField(max_length=100)
     link = models.UrlField()
-    image = modles.ImageField()
+    image = models.ImageField()
+
 
 class ResourceIndexPage(Page):
     intro = RichTextField(blank=True)
-    
+
     content_panels = Page.content_panels + [
         FieldPanel('intro', classname="full")
     ]
+
 
 class ResourcePage(Page):
     date = models.DateField("Post Date")
